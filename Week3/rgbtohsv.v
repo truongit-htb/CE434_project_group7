9c3c1907e0c7cf66ad2c5c1aeed66d0d553bc5fe
truongit-htb <80138969+truongit-htb@users.noreply.github.com>
2021-03-27 08:42:41 +0700

    update datapath and rgbtohsv

--- Week3/rgbtohsv.v
+++ Week3/rgbtohsv.v
@@ -1,12 +1,13 @@
-module rgbtohsv(H, S, V, Out_FLag, In, clk);
+module rgbtohsv(H, S, V, Out_Valid, In_Valid, In, rst_n, clk);
 output [31:0] H, S, V;
-output Out_FLag;
-input clk;
+output Out_Valid;
+//output Out_FLag;
+input clk, rst_n, In_Valid;
 input [95:0] In;
-wire  Valid, S0, Cmax_En, Cmin_En, delta_En;
-wire [2:0] tri_En, reg_En;
+wire  Valid; //S0, Cmax_En, Cmin_En, delta_En;
+//wire [2:0] tri_En;
 
-Control ins0(Valid, S0, Cmax_En, Cmin_En, delta_En, tri_En, reg_En, Out_FLag, clk);
-DataPath ins1(H, S, V, In, Valid, clk, S0, Cmax_En, Cmin_En, delta_En, tri_En, reg_En);
+Control ins0(Out_Valid, In_Valid, rst_n, clk);
+DataPath ins1(H, S, V, In, clk);
 
 endmodule
