cb67d9383eb25b0cb25a82909dc11d01c89b7005
truongit-htb <truongit.htb@gmail.com>
2021-07-01 14:27:16 +0700

    add FP_Top_AddSub.v

---
+++ Project_VGG16/Verilog/rtl/FP_Top_AddSub.v
@@ -0,0 +1,37 @@
+`include "FP_cong.v"
+// `include "delay_clock.v"
+
+module FP_Top_AddSub (
+    input  clk, resetn, valid_in,
+    input  [31:0] a, b,
+    output [31:0] out,
+    output valid_out    
+);
+    reg [31:0] in1, in2;
+
+    always @(posedge clk or negedge resetn) 
+    begin
+        if (resetn == 1'b0)
+        begin
+            in1 <= 32'b0;
+            in2 <= 32'b0;
+        end 
+		else 
+            if(valid_in == 1'b1)
+            begin
+                in1 <= a;
+                in2 <= b;
+            end 
+            else 
+            // if (valid_in == 1'b0) 
+            begin
+                in1 <= in1;
+                in2 <= in2;
+            end
+    end
+
+    FP_cong cong_fp (out, in1, in2);
+
+    delay_clock #(.DATA_WIDTH(1),.N_CLOCKs(1)) valid_inst(clk, resetn, 1'b1, valid_in, valid_out);
+
+endmodule
