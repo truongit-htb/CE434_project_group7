8409b569eabd6bb7b1a43540da202cc309725c0b
truongit-htb <truongit.htb@gmail.com>
2021-07-16 12:25:19 +0700

    update max4input.v

--- Project_VGG16/Verilog/rtl/max4input.v
+++ Project_VGG16/Verilog/rtl/max4input.v
@@ -13,116 +13,67 @@ module max4input #(
     input [DATA_WIDTH-1:0] in3;
     input [DATA_WIDTH-1:0] in4;
     output [DATA_WIDTH-1:0] out;
-    output valid_out;
+    output reg valid_out;
 
-    //reg [31:0] A,B,C,D;
     reg [DATA_WIDTH-1:0] maxab,maxcd,max;
     reg [1:0] counter,count_out;
-    //reg valid_r;
     assign out = max;
-    //reg enable;
-   // wire enable;
-    //assign enable = (counter > 2'b0)?1'b1:1'b0;
 
 
-    delay_clock #(.DATA_WIDTH(1),.N_CLOCKs(2)) valid_ins(clk,resetn,1'b1,valid_in,valid_out);
+    // delay_clock #(.DATA_WIDTH(1),.N_CLOCKs(2)) valid_ins(clk,resetn,1'b1,valid_in,valid_out);
     wire stage1;
     delay_clock #(.DATA_WIDTH(1),.N_CLOCKs(1)) st1_ins(clk,resetn,1'b1,valid_in,stage1);
-    // /*
-    // always @(posedge clk or negedge resetn) begin
-	// 	if (resetn == 1'b0) begin 
-    //             counter <= 2'd0;
-    //             valid_r<=1'b0;
-    //         end
-	// 	else if(valid_in == 1'b1) begin
-    //             counter <= 2'd3;
-    //             valid_r<=1'b1;
-    //         end
-    //     else if (valid_in == 1'b0) begin
-    //             if(counter == 2'b0) begin
-    //                 counter <= 0;
-    //                 valid_r<=1'b0;
-    //             end
-	// 		    else begin  
-    //                 counter <= counter - 2'b1;
-    //                 valid_r<=1'b0;
-    //             end
-    //         end
-    // end
-    // */
-	
-    /*
-    always @ (posedge clk or negedge resetn) begin
-        if (resetn == 1'b0) begin
-            A <= 0;
-            B <= 0;
-            C <= 0;
-            D <= 0;
-        end
-        else if (valid_in == 1'b1) begin
-            A <= in1;
-            B <= in2;
-            C <= in3;
-            D <= in4;
-        end 
-        else if (valid_in == 1'b0) begin
-            A <= A;
-            B <= B;
-            C <= C;
-            D <= D;
-        end 
-    end
-    */
-    
-    always @ (posedge clk or negedge resetn) begin
-        if (resetn == 1'b0) begin
+
+
+    always @ (posedge clk or negedge resetn) 
+    begin
+        if (resetn == 1'b0) 
             maxab <= 0;
-            //stage1 <= 0;
-        end
-        else if (valid_in == 1'b1) begin
-            if ( $signed(in1) > $signed(in2) ) begin
-              maxab <= in1;
-            end
-            else maxab <= in2;
-        end 
-        else if (valid_in == 1'b0) begin
-            maxab <= maxab;
-        end 
+        else 
+            if (valid_in == 1'b1) 
+                if ( $signed(in1) > $signed(in2) )
+                    maxab <= in1;
+                else 
+                    maxab <= in2;
+            else 
+                maxab <= maxab;
     end
 
-    always @ (posedge clk or negedge resetn) begin
-        if (resetn == 1'b0) begin
+    always @ (posedge clk or negedge resetn) 
+    begin
+        if (resetn == 1'b0)
             maxcd <= 0;
-        end
-        else if (valid_in == 1'b1) begin
-            if ( $signed(in3) > $signed(in4) ) begin
-              maxcd <= in3;
-            end
-            else maxcd <= in4;
-        end 
-        else if (valid_in == 1'b0) begin
-            maxcd <= maxcd;
-        end 
+        else 
+            if (valid_in == 1'b1) 
+                if ( $signed(in3) > $signed(in4) ) 
+                    maxcd <= in3;
+                else
+                    maxcd <= in4;
+            else 
+                maxcd <= maxcd;
     end
 
-    always @ (posedge clk or negedge resetn) begin
-        if (resetn == 1'b0) begin
+    always @ (posedge clk or negedge resetn) 
+    begin
+        if (resetn == 1'b0) 
+        begin
             max <= 0;
-            //valid_out <= 0;
+            valid_out <= 1'b0;
         end
-        else if (stage1 == 1'b1) begin
-            if ( $signed(maxab) >= $signed(maxcd) ) begin
-              max <= maxab;
-              //valid_out <= 1;
+        else 
+            if (stage1 == 1'b1)
+            begin
+                if ( $signed(maxab) >= $signed(maxcd) ) 
+                    max <= maxab;
+                else 
+                    max <= maxcd;
+                valid_out <= 1'b1; 
             end
-            else begin
-                max <= maxcd;
-                //valid_out <= 1;
+            else 
+            // if (stage1 == 1'b0) 
+            begin
+                max <= max;
+                valid_out <= 1'b0;
             end 
-        end 
-        else if (stage1 == 1'b0) begin
-            max <= max;
-            //valid_out <= valid_out;
-        end 
     end
 endmodule
