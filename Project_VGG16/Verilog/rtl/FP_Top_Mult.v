e7f7ebc0cae7cef7d1cd9490f1931cb87662e7db
truongit-htb <truongit.htb@gmail.com>
2021-07-16 12:22:23 +0700

    update FP_Top_Mult.v

--- Project_VGG16/Verilog/rtl/FP_Top_Mult.v
+++ Project_VGG16/Verilog/rtl/FP_Top_Mult.v
@@ -4,33 +4,39 @@
 module FP_Top_Mult (
     input  clk, resetn, valid_in,
     input  [31:0] a, b,
-    output [31:0] out,
+    output reg [31:0] out,
     output valid_out    
 );
-    reg [31:0] in1, in2;
+    // reg [31:0] in1, in2;
+    wire [31:0] out_mult;
+
+    FP_nhan nhan_fp (out_mult, a, b);
+
 
     always @(posedge clk or negedge resetn) 
     begin
         if (resetn == 1'b0)
         begin
-            in1 <= 32'b0;
-            in2 <= 32'b0;
+            out <= 32'b0;
+            // in1 <= 32'b0;
+            // in2 <= 32'b0;
         end 
 		else 
             if(valid_in == 1'b1)
             begin
-                in1 <= a;
-                in2 <= b;
+                out <= out_mult;
+                // in1 <= a;
+                // in2 <= b;
             end 
             else 
             // if (valid_in == 1'b0) 
             begin
-                in1 <= in1;
-                in2 <= in2;
+                out <= out;
+                // in1 <= in1;
+                // in2 <= in2;
             end
     end
 
-    FP_nhan nhan_fp (out, in1, in2);
 
     delay_clock #(.DATA_WIDTH(1),.N_CLOCKs(1)) valid_inst(clk, resetn, 1'b1, valid_in, valid_out);
 
