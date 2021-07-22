c84e8ffd2ae1dba63bbbb223a52ca92946472b1c
truongit-htb <truongit.htb@gmail.com>
2021-07-23 01:08:16 +0700

    update Verilog/rtl/conv3d_kernel_8_channel_size_3.v

--- Project_VGG16/Verilog/rtl/conv3d_kernel_8_channel_size_3.v
+++ Project_VGG16/Verilog/rtl/conv3d_kernel_8_channel_size_3.v
@@ -1,299 +1,303 @@
 `ifndef CONV_2D_KERNEL_SIZE_3_INCLUDED
-  `include "conv2d_kernel_size_3.v"
-  `define CONV_2D_KERNEL_SIZE_3_INCLUDED
+    `include "conv2d_kernel_size_3.v"
+    `define CONV_2D_KERNEL_SIZE_3_INCLUDED
 `endif
 
 module conv3d_kernel_8_channel_size_3 #(
-  parameter DATA_WIDTH = 32,
-  parameter IMG_WIDTH = 56,
-  parameter IMG_HEIGHT = 56,
-  parameter SIZE = 3,
-  parameter CHANNEL = 8
-  )
-  (
-    input     wire            clk,
-    input     wire            resetn,
-    // input     wire            eof,
-    input     wire            data_valid_in,
-    input     wire   [DATA_WIDTH-1:0]    data_in0,
-input     wire   [DATA_WIDTH-1:0]    data_in1,
-input     wire   [DATA_WIDTH-1:0]    data_in2,
-input     wire   [DATA_WIDTH-1:0]    data_in3,
-input     wire   [DATA_WIDTH-1:0]    data_in4,
-input     wire   [DATA_WIDTH-1:0]    data_in5,
-input     wire   [DATA_WIDTH-1:0]    data_in6,
-input     wire   [DATA_WIDTH-1:0]    data_in7,
+    parameter DATA_WIDTH = 32,
+    parameter IMG_WIDTH = 56,
+    parameter IMG_HEIGHT = 56,
+    parameter CHANNEL = 8,
 
-    // input     wire   [DATA_WIDTH-1:0]    data_in,
-    input     wire            load_kernel,
-    input     wire   [31:0]   kernel,
-    //input     wire            enable_cvt,
+    // Cx_Wy = CHANNELx_WEIGHTy
     
-    output    wire   [31:0]    data_out,
-    output     valid_out_pixel, // bi tat sau W*H - (W-2)*(H-2)
-    output reg done_img,
-	output reg load_kernel_done
-  );
-    localparam NUM_OPERANDs = CHANNEL+1;
-    localparam NUM_DELAY = 0;
-    
-    reg [31:0] counter_kernel;
-    always @ (posedge clk or negedge resetn)begin
-      if(resetn == 0) counter_kernel <= 0;
-      else if (load_kernel == 1'b1) begin
-        if(counter_kernel > SIZE*SIZE*CHANNEL+CHANNEL) counter_kernel <= counter_kernel;
-        else counter_kernel <= counter_kernel + 1;
-      end
-      else counter_kernel <= counter_kernel;
-    end
+	parameter C0_W0 = 32'h0, C0_W1 = 32'h0, C0_W2 = 32'h0, C0_W3 = 32'h0, C0_W4 = 32'h0, C0_W5 = 32'h0, C0_W6 = 32'h0, C0_W7 = 32'h0, C0_W8 = 32'h0,
+	parameter C1_W0 = 32'h0, C1_W1 = 32'h0, C1_W2 = 32'h0, C1_W3 = 32'h0, C1_W4 = 32'h0, C1_W5 = 32'h0, C1_W6 = 32'h0, C1_W7 = 32'h0, C1_W8 = 32'h0,
+	parameter C2_W0 = 32'h0, C2_W1 = 32'h0, C2_W2 = 32'h0, C2_W3 = 32'h0, C2_W4 = 32'h0, C2_W5 = 32'h0, C2_W6 = 32'h0, C2_W7 = 32'h0, C2_W8 = 32'h0,
+	parameter C3_W0 = 32'h0, C3_W1 = 32'h0, C3_W2 = 32'h0, C3_W3 = 32'h0, C3_W4 = 32'h0, C3_W5 = 32'h0, C3_W6 = 32'h0, C3_W7 = 32'h0, C3_W8 = 32'h0,
+	parameter C4_W0 = 32'h0, C4_W1 = 32'h0, C4_W2 = 32'h0, C4_W3 = 32'h0, C4_W4 = 32'h0, C4_W5 = 32'h0, C4_W6 = 32'h0, C4_W7 = 32'h0, C4_W8 = 32'h0,
+	parameter C5_W0 = 32'h0, C5_W1 = 32'h0, C5_W2 = 32'h0, C5_W3 = 32'h0, C5_W4 = 32'h0, C5_W5 = 32'h0, C5_W6 = 32'h0, C5_W7 = 32'h0, C5_W8 = 32'h0,
+	parameter C6_W0 = 32'h0, C6_W1 = 32'h0, C6_W2 = 32'h0, C6_W3 = 32'h0, C6_W4 = 32'h0, C6_W5 = 32'h0, C6_W6 = 32'h0, C6_W7 = 32'h0, C6_W8 = 32'h0,
+	parameter C7_W0 = 32'h0, C7_W1 = 32'h0, C7_W2 = 32'h0, C7_W3 = 32'h0, C7_W4 = 32'h0, C7_W5 = 32'h0, C7_W6 = 32'h0, C7_W7 = 32'h0, C7_W8 = 32'h0,
+	parameter BIAS  = 32'h0
+    )
+    (
+    input	clk,
+	input	resetn,
+	input	data_valid_in,
+	input	[DATA_WIDTH-1:0]	data_in_0,
+	input	[DATA_WIDTH-1:0]	data_in_1,
+	input	[DATA_WIDTH-1:0]	data_in_2,
+	input	[DATA_WIDTH-1:0]	data_in_3,
+	input	[DATA_WIDTH-1:0]	data_in_4,
+	input	[DATA_WIDTH-1:0]	data_in_5,
+	input	[DATA_WIDTH-1:0]	data_in_6,
+	input	[DATA_WIDTH-1:0]	data_in_7,
+	output	[DATA_WIDTH-1:0]	data_out,
+	output	valid_out_pixel,
+	output	done
+    );
+    localparam NUM_OPERANDs = CHANNEL;
+    localparam NUM_DELAY = 0;           
 
-    wire load_kernel0,load_kernel1,load_kernel2,load_kernel3,load_kernel4,load_kernel5,load_kernel6,load_kernel7;
-    wire load_bias;
-    reg [31:0] bias;
-    reg [31:0] counter;
-
+    reg  [31:0] counter;
     wire [31:0] data_out_conv_kernel [0:CHANNEL-1];
+    wire [CHANNEL-1:0] valid_out_conv_kernel;
+    wire [CHANNEL-1:0] done_conv;
 
-    assign load_kernel0 = ((counter_kernel >= 0 & counter_kernel < 10) & load_kernel == 1'b1)?1'b1:1'b0;
-assign load_kernel1 = ((counter_kernel >= 10 & counter_kernel < 20) & load_kernel == 1'b1)?1'b1:1'b0;
-assign load_kernel2 = ((counter_kernel >= 20 & counter_kernel < 30) & load_kernel == 1'b1)?1'b1:1'b0;
-assign load_kernel3 = ((counter_kernel >= 30 & counter_kernel < 40) & load_kernel == 1'b1)?1'b1:1'b0;
-assign load_kernel4 = ((counter_kernel >= 40 & counter_kernel < 50) & load_kernel == 1'b1)?1'b1:1'b0;
-assign load_kernel5 = ((counter_kernel >= 50 & counter_kernel < 60) & load_kernel == 1'b1)?1'b1:1'b0;
-assign load_kernel6 = ((counter_kernel >= 60 & counter_kernel < 70) & load_kernel == 1'b1)?1'b1:1'b0;
-assign load_kernel7 = ((counter_kernel >= 70 & counter_kernel < 80) & load_kernel == 1'b1)?1'b1:1'b0;
-assign load_bias = (counter_kernel == 80 & load_kernel == 1'b1)?1'b1:1'b0;
-
-
-    // always @ (posedge clk or negedge resetn) begin
-    //   if(resetn == 0) begin
-    //     <load_kernel_resetn>load_bias <= 1'b0;
-    //   end
-    //   else
-    //   <load_kernel>
-    //   if(counter_kernel == SIZE*SIZE*CHANNEL) begin
-    //   <load_kernel_resetn>load_bias <= 1'b1;
-
-    //   end else begin
-    //   <load_kernel_resetn>load_bias <= 1'b0;
-    //   end
-    // end
-    always @(posedge clk or negedge resetn) begin
-      if(resetn == 0)begin
-        load_kernel_done <=0;
-        bias <= 0;
-      end
-      else if(load_bias == 1'b1)begin
-        load_kernel_done <=load_bias;
-        bias <= kernel;
-        end
-      else begin
-        load_kernel_done <=load_bias;
-        bias <= bias;
-      end
-    end
-
-    conv2d_kernel_size_3 #(
-		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT)
-		)
-		conv2_0(
-		clk,
-		resetn,
-		data_vaid_in,
-		data_in0,             
-		load_kernel0,
-		kernel,            
-		data_out_conv_kernel[0],
-		valid_out_conv_kernel_0,
-		done_conv_0,
-		load_kernel_done_0
-		);
-conv2d_kernel_size_3 #(
-		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT)
-		)
-		conv2_1(
-		clk,
-		resetn,
-		data_vaid_in,
-		data_in1,             
-		load_kernel1,
-		kernel,            
-		data_out_conv_kernel[1],
-		valid_out_conv_kernel_1,
-		done_conv_1,
-		load_kernel_done_1
-		);
-conv2d_kernel_size_3 #(
-		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT)
-		)
-		conv2_2(
-		clk,
-		resetn,
-		data_vaid_in,
-		data_in2,             
-		load_kernel2,
-		kernel,            
-		data_out_conv_kernel[2],
-		valid_out_conv_kernel_2,
-		done_conv_2,
-		load_kernel_done_2
-		);
-conv2d_kernel_size_3 #(
-		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT)
-		)
-		conv2_3(
-		clk,
-		resetn,
-		data_vaid_in,
-		data_in3,             
-		load_kernel3,
-		kernel,            
-		data_out_conv_kernel[3],
-		valid_out_conv_kernel_3,
-		done_conv_3,
-		load_kernel_done_3
-		);
-conv2d_kernel_size_3 #(
-		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT)
-		)
-		conv2_4(
-		clk,
-		resetn,
-		data_vaid_in,
-		data_in4,             
-		load_kernel4,
-		kernel,            
-		data_out_conv_kernel[4],
-		valid_out_conv_kernel_4,
-		done_conv_4,
-		load_kernel_done_4
-		);
-conv2d_kernel_size_3 #(
-		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT)
-		)
-		conv2_5(
-		clk,
-		resetn,
-		data_vaid_in,
-		data_in5,             
-		load_kernel5,
-		kernel,            
-		data_out_conv_kernel[5],
-		valid_out_conv_kernel_5,
-		done_conv_5,
-		load_kernel_done_5
-		);
-conv2d_kernel_size_3 #(
-		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT)
-		)
-		conv2_6(
-		clk,
-		resetn,
-		data_vaid_in,
-		data_in6,             
-		load_kernel6,
-		kernel,            
-		data_out_conv_kernel[6],
-		valid_out_conv_kernel_6,
-		done_conv_6,
-		load_kernel_done_6
-		);
-conv2d_kernel_size_3 #(
-		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT)
-		)
-		conv2_7(
-		clk,
-		resetn,
-		data_vaid_in,
-		data_in7,             
-		load_kernel7,
-		kernel,            
-		data_out_conv_kernel[7],
-		valid_out_conv_kernel_7,
-		done_conv_7,
-		load_kernel_done_7
-		);
-
-
-  wire [SIZE*SIZE-2:0] valid_in_add;
-    wire [SIZE*SIZE-2:0] valid_out_add;
-    wire [31:0] output_add [0:SIZE*SIZE-2];
-    wire [31:0] op_1 [0:SIZE*SIZE-2];
-    wire [31:0] op_2 [0:SIZE*SIZE-2];
-genvar i;
-    generate
-    for (i = 0; i < NUM_OPERANDs-1; i=i+1) 
-    begin :initial_adders // needs (NUM_OPERANDs - 1) adders
-      //j=j+1;
-      fpadd fp_adders(clk,resetn,valid_in_add[i],op_1[i],op_2[i],output_add[i],valid_out_add[i]);
-    end
-    endgenerate
+    wire [NUM_OPERANDs-2:0] valid_in_add;
+    wire [NUM_OPERANDs-2:0] valid_out_add;
+    wire [31:0] output_add [0:NUM_OPERANDs-2];
+    wire [31:0] op_1 [0:NUM_OPERANDs-2];
+    wire [31:0] op_2 [0:NUM_OPERANDs-2];
 
     wire [31:0] in_delay [0:NUM_DELAY-1];
-  wire [31:0] out_delay [0:NUM_DELAY-1];
-  generate 
-    for (i = 0; i < NUM_DELAY; i=i+1) 
-    begin :delay_clocksssss
-      delay_clock #(.DATA_WIDTH(32),.N_CLOCKs(6)) delay_xxx(clk,resetn,enable_2,in_delay[i],out_delay[i]);
-	end
-	endgenerate
-    assign op_1[0] =data_out_conv_kernel[0];
-assign op_2[0] = data_out_conv_kernel[1];
-assign valid_in_add[0] = valid_out_conv_kernel_7;
-assign op_1[1] =data_out_conv_kernel[2];
-assign op_2[1] = data_out_conv_kernel[3];
-assign valid_in_add[1] = valid_out_conv_kernel_7;
-assign op_1[2] =data_out_conv_kernel[4];
-assign op_2[2] = data_out_conv_kernel[5];
-assign valid_in_add[2] = valid_out_conv_kernel_7;
-assign op_1[3] =data_out_conv_kernel[6];
-assign op_2[3] = data_out_conv_kernel[7];
-assign valid_in_add[3] = valid_out_conv_kernel_7;
-assign op_1[4] = output_add[0];
-assign op_2[4] = output_add[1];
-assign valid_in_add[4] = valid_out_add[1];
-assign op_1[5] = output_add[2];
-assign op_2[5] = output_add[3];
-assign valid_in_add[5] = valid_out_add[3];
-assign op_1[6] = output_add[4];
-assign op_2[6] = output_add[5];
-assign valid_in_add[6] = valid_out_add[5];
+    wire [31:0] out_delay [0:NUM_DELAY-1];
 
+    
+	conv2d_kernel_size_3 #(
+		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT),
+		.kernelR0(C0_W0),
+		.kernelR1(C0_W1),
+		.kernelR2(C0_W2),
+		.kernelR3(C0_W3),
+		.kernelR4(C0_W4),
+		.kernelR5(C0_W5),
+		.kernelR6(C0_W6),
+		.kernelR7(C0_W7),
+		.kernelR8(C0_W8)
+		)
+		conv2d_0(
+		.clk(clk),
+		.resetn(resetn),
+		.data_valid_in(data_valid_in),
+		.data_in(data_in_0),
+		.data_out(data_out_conv_kernel[0]),
+		.valid_out_pixel(valid_out_conv_kernel[0]),
+		.done(done_conv[0])
+		);
+	conv2d_kernel_size_3 #(
+		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT),
+		.kernelR0(C1_W0),
+		.kernelR1(C1_W1),
+		.kernelR2(C1_W2),
+		.kernelR3(C1_W3),
+		.kernelR4(C1_W4),
+		.kernelR5(C1_W5),
+		.kernelR6(C1_W6),
+		.kernelR7(C1_W7),
+		.kernelR8(C1_W8)
+		)
+		conv2d_1(
+		.clk(clk),
+		.resetn(resetn),
+		.data_valid_in(data_valid_in),
+		.data_in(data_in_1),
+		.data_out(data_out_conv_kernel[1]),
+		.valid_out_pixel(valid_out_conv_kernel[1]),
+		.done(done_conv[1])
+		);
+	conv2d_kernel_size_3 #(
+		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT),
+		.kernelR0(C2_W0),
+		.kernelR1(C2_W1),
+		.kernelR2(C2_W2),
+		.kernelR3(C2_W3),
+		.kernelR4(C2_W4),
+		.kernelR5(C2_W5),
+		.kernelR6(C2_W6),
+		.kernelR7(C2_W7),
+		.kernelR8(C2_W8)
+		)
+		conv2d_2(
+		.clk(clk),
+		.resetn(resetn),
+		.data_valid_in(data_valid_in),
+		.data_in(data_in_2),
+		.data_out(data_out_conv_kernel[2]),
+		.valid_out_pixel(valid_out_conv_kernel[2]),
+		.done(done_conv[2])
+		);
+	conv2d_kernel_size_3 #(
+		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT),
+		.kernelR0(C3_W0),
+		.kernelR1(C3_W1),
+		.kernelR2(C3_W2),
+		.kernelR3(C3_W3),
+		.kernelR4(C3_W4),
+		.kernelR5(C3_W5),
+		.kernelR6(C3_W6),
+		.kernelR7(C3_W7),
+		.kernelR8(C3_W8)
+		)
+		conv2d_3(
+		.clk(clk),
+		.resetn(resetn),
+		.data_valid_in(data_valid_in),
+		.data_in(data_in_3),
+		.data_out(data_out_conv_kernel[3]),
+		.valid_out_pixel(valid_out_conv_kernel[3]),
+		.done(done_conv[3])
+		);
+	conv2d_kernel_size_3 #(
+		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT),
+		.kernelR0(C4_W0),
+		.kernelR1(C4_W1),
+		.kernelR2(C4_W2),
+		.kernelR3(C4_W3),
+		.kernelR4(C4_W4),
+		.kernelR5(C4_W5),
+		.kernelR6(C4_W6),
+		.kernelR7(C4_W7),
+		.kernelR8(C4_W8)
+		)
+		conv2d_4(
+		.clk(clk),
+		.resetn(resetn),
+		.data_valid_in(data_valid_in),
+		.data_in(data_in_4),
+		.data_out(data_out_conv_kernel[4]),
+		.valid_out_pixel(valid_out_conv_kernel[4]),
+		.done(done_conv[4])
+		);
+	conv2d_kernel_size_3 #(
+		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT),
+		.kernelR0(C5_W0),
+		.kernelR1(C5_W1),
+		.kernelR2(C5_W2),
+		.kernelR3(C5_W3),
+		.kernelR4(C5_W4),
+		.kernelR5(C5_W5),
+		.kernelR6(C5_W6),
+		.kernelR7(C5_W7),
+		.kernelR8(C5_W8)
+		)
+		conv2d_5(
+		.clk(clk),
+		.resetn(resetn),
+		.data_valid_in(data_valid_in),
+		.data_in(data_in_5),
+		.data_out(data_out_conv_kernel[5]),
+		.valid_out_pixel(valid_out_conv_kernel[5]),
+		.done(done_conv[5])
+		);
+	conv2d_kernel_size_3 #(
+		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT),
+		.kernelR0(C6_W0),
+		.kernelR1(C6_W1),
+		.kernelR2(C6_W2),
+		.kernelR3(C6_W3),
+		.kernelR4(C6_W4),
+		.kernelR5(C6_W5),
+		.kernelR6(C6_W6),
+		.kernelR7(C6_W7),
+		.kernelR8(C6_W8)
+		)
+		conv2d_6(
+		.clk(clk),
+		.resetn(resetn),
+		.data_valid_in(data_valid_in),
+		.data_in(data_in_6),
+		.data_out(data_out_conv_kernel[6]),
+		.valid_out_pixel(valid_out_conv_kernel[6]),
+		.done(done_conv[6])
+		);
+	conv2d_kernel_size_3 #(
+		.DATA_WIDTH(32),.IMG_WIDTH(IMG_WIDTH),.IMG_HEIGHT(IMG_HEIGHT),
+		.kernelR0(C7_W0),
+		.kernelR1(C7_W1),
+		.kernelR2(C7_W2),
+		.kernelR3(C7_W3),
+		.kernelR4(C7_W4),
+		.kernelR5(C7_W5),
+		.kernelR6(C7_W6),
+		.kernelR7(C7_W7),
+		.kernelR8(C7_W8)
+		)
+		conv2d_7(
+		.clk(clk),
+		.resetn(resetn),
+		.data_valid_in(data_valid_in),
+		.data_in(data_in_7),
+		.data_out(data_out_conv_kernel[7]),
+		.valid_out_pixel(valid_out_conv_kernel[7]),
+		.done(done_conv[7])
+		);
+
+
+    
+	assign op_1[0] = data_out_conv_kernel[0];
+	assign op_2[0] = data_out_conv_kernel[1];
+	assign valid_in_add[0] = valid_out_conv_kernel[7];
+	assign op_1[1] = data_out_conv_kernel[2];
+	assign op_2[1] = data_out_conv_kernel[3];
+	assign valid_in_add[1] = valid_out_conv_kernel[7];
+	assign op_1[2] = data_out_conv_kernel[4];
+	assign op_2[2] = data_out_conv_kernel[5];
+	assign valid_in_add[2] = valid_out_conv_kernel[7];
+	assign op_1[3] = data_out_conv_kernel[6];
+	assign op_2[3] = data_out_conv_kernel[7];
+	assign valid_in_add[3] = valid_out_conv_kernel[7];
+	assign op_1[4] = output_add[0];
+	assign op_2[4] = output_add[1];
+	assign valid_in_add[4] = valid_out_add[1];
+	assign op_1[5] = output_add[2];
+	assign op_2[5] = output_add[3];
+	assign valid_in_add[5] = valid_out_add[3];
+	assign op_1[6] = output_add[4];
+	assign op_2[6] = output_add[5];
+	assign valid_in_add[6] = valid_out_add[5];
+
+
+    // Tao ra (NUM_OPERANDs - 1) bo fp add
+    genvar i;
+    generate
+        for (i = 0; i < NUM_OPERANDs-1; i=i+1) 
+        begin :initial_adders // needs (NUM_OPERANDs - 1) adders
+            FP_Top_AddSub fp_adders(clk,resetn,valid_in_add[i],op_1[i],op_2[i],output_add[i],valid_out_add[i]);
+        end
+    endgenerate
+
+    // Delay input cho bo fp add
+    generate 
+        for (i = 0; i < NUM_DELAY; i=i+1) 
+        begin :delay_clocksssss
+            // ------------ Thay doi do delay theo mach cong ------------
+            delay_clock #(.DATA_WIDTH(32),.N_CLOCKs(1)) delay_xxx(clk,resetn,1'b1,in_delay[i],out_delay[i]);
+        end
+    endgenerate
+
+    
     wire valid_temp;
     wire [31:0] data_temp;
     assign valid_temp = valid_out_add[NUM_OPERANDs-2];
 	assign data_temp = output_add[NUM_OPERANDs-2];
 
-  wire temp_valid;
-  fpadd add_bias(clk,resetn,valid_temp,data_temp,bias,data_out,temp_valid);
+    wire temp_valid;
+    FP_Top_AddSub add_bias(clk, resetn, valid_temp, data_temp, BIAS, data_out, temp_valid);
 
-  assign valid_out_pixel = temp_valid;
+    assign valid_out_pixel = temp_valid;
 
-  
-	
-    always @ (posedge clk or negedge resetn) begin
-      if(resetn == 1'b0) counter <= 0;
-      else if (done_img == 1'b1) begin
-        counter <= 0;
-      end
-      else if(temp_valid == 1'b1) begin
-        if(counter == (IMG_WIDTH)*(IMG_HEIGHT) -1 ) counter <= 0;
-        else counter <= counter + 1;
-      end
-      else counter <= counter;
+
+    always @ (posedge clk or negedge resetn) 
+    begin
+        if(resetn == 1'b0)
+            counter <= 0;
+        else 
+            if (done == 1'b1) 
+                counter <= 0;
+            else 
+                if(valid_out_pixel == 1'b1)
+                begin
+                    if(counter == (IMG_WIDTH)*(IMG_HEIGHT) -1 )
+                    counter <= 0;
+                    else 
+                    counter <= counter + 1;
+                end
+                else
+                    counter <= counter;
     end
-    always @ (posedge clk or negedge resetn) begin
-      if(resetn == 1'b0) done_img <= 0;
-      else if(counter == (IMG_WIDTH)*(IMG_HEIGHT) -2) done_img <= (temp_valid)?1'b1:1'b0;
-      else done_img <= 0;
-    end
-
-
-    //float2int result(clk,resetn,enable,res8,valid_add8,data_out,valid_out_pixel);
     
-  
-
-  
-
+    assign done = (counter == (IMG_WIDTH)*(IMG_HEIGHT) -1) & valid_out_pixel;
 
   endmodule
