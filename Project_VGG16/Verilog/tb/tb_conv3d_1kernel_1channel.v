4aafa60ccc852852b744079dc0056fced19652a1
truongit-htb <truongit.htb@gmail.com>
2021-07-23 17:04:41 +0700

    update Verilog/tb/tb_conv3d_1kernel_1channel.v

--- Project_VGG16/Verilog/tb/tb_conv3d_1kernel_1channel.v
+++ Project_VGG16/Verilog/tb/tb_conv3d_1kernel_1channel.v
@@ -8,24 +8,43 @@ module tb_conv3d_1kernel_1channel;
     // parameter Image_Channel1 = "../Data/3_data_in/data_fp_sun_01_channel_001.txt";
     // parameter Image_Channel2 = "../Data/3_data_in/data_fp_sun_01_channel_002.txt";
     // // EXTEND
-    parameter Image_Channel0 = "../Data/4_data_out/modelsim_block1_conv1_sun_000.txt";
-    parameter Image_Channel1 = "../Data/4_data_out/modelsim_block1_conv1_sun_001.txt";
-    parameter Image_Channel2 = "../Data/4_data_out/modelsim_block1_conv1_sun_002.txt";
-    parameter Image_Channel3 = "../Data/4_data_out/modelsim_block1_conv1_sun_003.txt";
-    parameter Image_Channel4 = "../Data/4_data_out/modelsim_block1_conv1_sun_004.txt";
-    parameter Image_Channel5 = "../Data/4_data_out/modelsim_block1_conv1_sun_005.txt";
-    parameter Image_Channel6 = "../Data/4_data_out/modelsim_block1_conv1_sun_006.txt";
-    parameter Image_Channel7 = "../Data/4_data_out/modelsim_block1_conv1_sun_007.txt";
+    parameter Image_Channel0 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_000.txt";
+    parameter Image_Channel1 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_001.txt";
+    parameter Image_Channel2 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_002.txt";
+    parameter Image_Channel3 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_003.txt";
+    parameter Image_Channel4 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_004.txt";
+    parameter Image_Channel5 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_005.txt";
+    parameter Image_Channel6 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_006.txt";
+    parameter Image_Channel7 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_007.txt";
+
+    parameter Image_Channel8  = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_008.txt";
+    parameter Image_Channel9  = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_009.txt";
+    parameter Image_Channel10 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_0010.txt";
+    parameter Image_Channel11 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_0011.txt";
+    parameter Image_Channel12 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_0012.txt";
+    parameter Image_Channel13 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_0013.txt";
+    parameter Image_Channel14 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_0014.txt";
+    parameter Image_Channel15 = "../Data/4_data_out/block4_conv1_temp/modelsim_temp_block4_conv1_sun_0015.txt";
     
 
-    parameter Outfile_0   = "../Data/4_data_out/modelsim_block1_conv2_sun_01_000.txt";
-    parameter Outfile_1   = "../Data/4_data_out/modelsim_block1_conv2_sun_01_001.txt";
-    parameter Outfile_2   = "../Data/4_data_out/modelsim_block1_conv2_sun_01_002.txt";
-    parameter Outfile_3   = "../Data/4_data_out/modelsim_block1_conv2_sun_01_003.txt";
-    parameter Outfile_4   = "../Data/4_data_out/modelsim_block1_conv2_sun_01_004.txt";
-    parameter Outfile_5   = "../Data/4_data_out/modelsim_block1_conv2_sun_01_005.txt";
-    parameter Outfile_6   = "../Data/4_data_out/modelsim_block1_conv2_sun_01_006.txt";
-    parameter Outfile_7   = "../Data/4_data_out/modelsim_block1_conv2_sun_01_007.txt";
+
+    parameter Outfile_0   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_000.txt";
+    parameter Outfile_1   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_001.txt";
+    parameter Outfile_2   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_002.txt";
+    parameter Outfile_3   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_003.txt";
+    parameter Outfile_4   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_004.txt";
+    parameter Outfile_5   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_005.txt";
+    parameter Outfile_6   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_006.txt";
+    parameter Outfile_7   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_007.txt";
+    // // EXTEND
+    parameter Outfile_8    = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_008.txt";
+    parameter Outfile_9    = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_009.txt";
+    parameter Outfile_10   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_0010.txt";
+    parameter Outfile_11   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_0011.txt";
+    parameter Outfile_12   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_0012.txt";
+    parameter Outfile_13   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_0013.txt";
+    parameter Outfile_14   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_0014.txt";
+    parameter Outfile_15   = "../Data/4_data_out/block4_conv2_temp/modelsim_temp_block4_conv2_sun_0015.txt";
 
     parameter k = 5;
 
@@ -44,7 +63,19 @@ module tb_conv3d_1kernel_1channel;
     wire [31:0] data_in_5;
     wire [31:0] data_in_6;
     wire [31:0] data_in_7;
-    wire [31:0] data_out;
+
+    wire [31:0] data_in_8 ;
+    wire [31:0] data_in_9 ;
+    wire [31:0] data_in_10;
+    wire [31:0] data_in_11;
+    wire [31:0] data_in_12;
+    wire [31:0] data_in_13;
+    wire [31:0] data_in_14;
+    wire [31:0] data_in_15;
+
+
+
+    // wire [31:0] data_out;
     wire valid_out, done;
 
     wire   [DATA_WIDTH-1:0]    data_out_0;
@@ -55,13 +86,20 @@ module tb_conv3d_1kernel_1channel;
     wire   [DATA_WIDTH-1:0]    data_out_5;
     wire   [DATA_WIDTH-1:0]    data_out_6;
     wire   [DATA_WIDTH-1:0]    data_out_7;
-
+    // // EXTEND
+    wire   [DATA_WIDTH-1:0]    data_out_8 ;
+    wire   [DATA_WIDTH-1:0]    data_out_9 ;
+    wire   [DATA_WIDTH-1:0]    data_out_10;
+    wire   [DATA_WIDTH-1:0]    data_out_11;
+    wire   [DATA_WIDTH-1:0]    data_out_12;
+    wire   [DATA_WIDTH-1:0]    data_out_13;
+    wire   [DATA_WIDTH-1:0]    data_out_14;
+    wire   [DATA_WIDTH-1:0]    data_out_15;
 
     initial 
     begin
         clk = 1'b0;
-        resetn = 1'b0;
-        // valid_in = 1'b1;    
+        resetn = 1'b0;   
         #(k*3/2) resetn = 1'b1;
     end
 
@@ -80,6 +118,15 @@ module tb_conv3d_1kernel_1channel;
         .input_file_6(Image_Channel6),
         .input_file_7(Image_Channel7),
 
+        .input_file_8 (Image_Channel8 ),
+        .input_file_9 (Image_Channel9 ),
+        .input_file_10(Image_Channel10),
+        .input_file_11(Image_Channel11),
+        .input_file_12(Image_Channel12),
+        .input_file_13(Image_Channel13),
+        .input_file_14(Image_Channel14),
+        .input_file_15(Image_Channel15),
+
         .WIDTH(`IMG_WIDTH),
         .HEIGHT(`IMG_HEIGHT)
     ) generator (
@@ -96,37 +143,25 @@ module tb_conv3d_1kernel_1channel;
         .fifo_data_6(data_in_6),
         .fifo_data_7(data_in_7),
 
+        .fifo_data_8 (data_in_8 ),
+        .fifo_data_9 (data_in_9 ),
+        .fifo_data_10(data_in_10),
+        .fifo_data_11(data_in_11),
+        .fifo_data_12(data_in_12),
+        .fifo_data_13(data_in_13),
+        .fifo_data_14(data_in_14),
+        .fifo_data_15(data_in_15),
+
         .fifo_wrreq(valid_in)
     );
 
-    // block1_conv1_8_kernel_3_channel #(
-	// 	.DATA_WIDTH(32),.IMG_WIDTH(`IMG_WIDTH),.IMG_HEIGHT(`IMG_HEIGHT)
-    // )
-    // block1_conv1_8kernel_3channel(
-	// 	.clk(clk),
-	// 	.resetn(resetn),
-	// 	.data_valid_in(valid_in),
-	// 	.data_in0(data_in_0),             
-	// 	.data_in1(data_in_1),             
-	// 	.data_in2(data_in_2), 
 
-    //     .data_out_conv0(data_out_0),
-    //     .data_out_conv1(data_out_1),
-    //     .data_out_conv2(data_out_2),
-    //     .data_out_conv3(data_out_3),
-    //     .data_out_conv4(data_out_4),
-    //     .data_out_conv5(data_out_5),
-    //     .data_out_conv6(data_out_6),
-    //     .data_out_conv7(data_out_7),
+    // // Edit here
 
-	// 	.valid_out_pixel(valid_out),
-	// 	.done_img()
-	// 	);
-
-
-    // testbench cho mach conv2d_8kernel_3channel
-    // block1_conv1 #(
-    block1_conv2 #(
+    // // block1_conv1 #(
+    // block1_conv2 #(
+    // block4_conv1 #(
+    block4_conv2 #(
 		.DATA_WIDTH(32),
         .WIDTH(`IMG_WIDTH),
         .HEIGHT(`IMG_HEIGHT)
@@ -143,7 +178,17 @@ module tb_conv3d_1kernel_1channel;
 		.data_in_4(data_in_4),             
 		.data_in_5(data_in_5), 
         .data_in_6(data_in_6),             
-		.data_in_7(data_in_7), 
+		.data_in_7(data_in_7),
+
+        .data_in_8 (data_in_8 ),             
+		.data_in_9 (data_in_9 ),             
+		.data_in_10(data_in_10), 
+        .data_in_11(data_in_11),             
+		.data_in_12(data_in_12), 
+        .data_in_13(data_in_13),             
+		.data_in_14(data_in_14),             
+		.data_in_15(data_in_15), 
+
 
         .data_out_0(data_out_0),
         .data_out_1(data_out_1),
@@ -153,6 +198,17 @@ module tb_conv3d_1kernel_1channel;
         .data_out_5(data_out_5),
         .data_out_6(data_out_6),
         .data_out_7(data_out_7),
+        // // EXTEND
+        .data_out_8 (data_out_8 ),
+        .data_out_9 (data_out_9 ),
+        .data_out_10(data_out_10),
+        .data_out_11(data_out_11),
+        .data_out_12(data_out_12),
+        .data_out_13(data_out_13),
+        .data_out_14(data_out_14),
+        .data_out_15(data_out_15),
+
+
 
 		.valid_out(valid_out),
 		.done()
@@ -168,6 +224,15 @@ module tb_conv3d_1kernel_1channel;
         .output_file_5(Outfile_5),
         .output_file_6(Outfile_6),
         .output_file_7(Outfile_7),
+        // // EXTEND
+        .output_file_8 (Outfile_8 ),
+        .output_file_9 (Outfile_9 ),
+        .output_file_10(Outfile_10),
+        .output_file_11(Outfile_11),
+        .output_file_12(Outfile_12),
+        .output_file_13(Outfile_13),
+        .output_file_14(Outfile_14),
+        .output_file_15(Outfile_15),
 
         // .WIDTH(`IMG_WIDTH>>1),
         // .HEIGHT(`IMG_HEIGHT>>1)
@@ -177,7 +242,6 @@ module tb_conv3d_1kernel_1channel;
         .clk(clk),
         .resetn(resetn),
 
-        // .data_in(data_out),
         .data_in_0(data_out_0),
         .data_in_1(data_out_1),
         .data_in_2(data_out_2),
@@ -186,209 +250,19 @@ module tb_conv3d_1kernel_1channel;
         .data_in_5(data_out_5),
         .data_in_6(data_out_6),
         .data_in_7(data_out_7),
+        // // EXTEND
+        .data_in_8 (data_out_8 ),
+        .data_in_9 (data_out_9 ),
+        .data_in_10(data_out_10),
+        .data_in_11(data_out_11),
+        .data_in_12(data_out_12),
+        .data_in_13(data_out_13),
+        .data_in_14(data_out_14),
+        .data_in_15(data_out_15),
+
 
         .data_valid_in(valid_out),
         .done(done)
     );
 
 endmodule
-
-
-
-
-
-
-// --------------- ex-TESTBENCH -------------
-
-// module tb_conv3d_1kernel_1channel;
-//     // // path on Ubuntu
-//     parameter Image_Channel0 = "/home/truong/Desktop/git_vgg16/Data/data_fp_image_channel_000.txt";
-//     parameter Image_Channel1 = "/home/truong/Desktop/git_vgg16/Data/data_fp_image_channel_001.txt";
-//     parameter Image_Channel2 = "/home/truong/Desktop/git_vgg16/Data/data_fp_image_channel_002.txt";
-//     // parameter Outfile   = "/home/truong/Desktop/git_vgg16/Data/modelsim_conv3D_8kernel_3channel_000.txt";
-
-//     parameter Outfile_0   = "/home/truong/Desktop/git_vgg16/Data/modelsim_conv3D_8kernel_3channel_000.txt";
-//     parameter Outfile_1   = "/home/truong/Desktop/git_vgg16/Data/modelsim_conv3D_8kernel_3channel_001.txt";
-//     parameter Outfile_2   = "/home/truong/Desktop/git_vgg16/Data/modelsim_conv3D_8kernel_3channel_002.txt";
-//     parameter Outfile_3   = "/home/truong/Desktop/git_vgg16/Data/modelsim_conv3D_8kernel_3channel_003.txt";
-//     parameter Outfile_4   = "/home/truong/Desktop/git_vgg16/Data/modelsim_conv3D_8kernel_3channel_004.txt";
-//     parameter Outfile_5   = "/home/truong/Desktop/git_vgg16/Data/modelsim_conv3D_8kernel_3channel_005.txt";
-//     parameter Outfile_6   = "/home/truong/Desktop/git_vgg16/Data/modelsim_conv3D_8kernel_3channel_006.txt";
-//     parameter Outfile_7   = "/home/truong/Desktop/git_vgg16/Data/modelsim_conv3D_8kernel_3channel_007.txt";
-
-//     parameter k = 10;
-
-//     // LAM ON NHO DAT DUNG KICH THUOC ANH KHONG THI DUNG HOI HAN!
-//     parameter DATA_WIDTH = 32;
-//     parameter WIDTH = 20;
-//     parameter HEIGHT = 20;
-//     parameter Total_Pixel = WIDTH * HEIGHT;
-
-
-//     reg clk;
-//     reg resetn;
-//     reg valid_in;
-//     reg [31:0] data_in0;
-//     reg [31:0] data_in1;
-//     reg [31:0] data_in2;
-//     // wire [31:0] data_out;
-//     wire valid_out, done;
-
-//     wire   [DATA_WIDTH-1:0]    data_out0;
-//     wire   [DATA_WIDTH-1:0]    data_out1;
-//     wire   [DATA_WIDTH-1:0]    data_out2;
-//     wire   [DATA_WIDTH-1:0]    data_out3;
-//     wire   [DATA_WIDTH-1:0]    data_out4;
-//     wire   [DATA_WIDTH-1:0]    data_out5;
-//     wire   [DATA_WIDTH-1:0]    data_out6;
-//     wire   [DATA_WIDTH-1:0]    data_out7;
-
-//     reg [32-1:0] In_Memory_0 [0:Total_Pixel-1];
-//     reg [32-1:0] In_Memory_1 [0:Total_Pixel-1];
-//     reg [32-1:0] In_Memory_2 [0:Total_Pixel-1];
-
-//     initial 
-//     begin
-//         $readmemh(Image_Channel0, In_Memory_0);
-//         $readmemh(Image_Channel1, In_Memory_1);
-//         $readmemh(Image_Channel2, In_Memory_2);
-//     end
-
-//     initial 
-//     begin
-//         clk = 1'b0;
-//         resetn = 1'b0;
-//         valid_in = 1'b1;    
-//         #(k*3/2) resetn = 1'b1;
-//     end
-
-//     integer i;
-//     initial 
-//     begin 
-//         //#(k*2) data_in = In_Memory[i];
-//         #(k*3/2);
-//         for (i = 0; i < Total_Pixel; i = i + 1) 
-//         begin
-            
-//             // if (i == 4)
-//             // valid_in = 1'b0; 
-//             // if (i == 6)
-//             // valid_in = 1'b1; 
-
-//             // if (i == 10)
-//             // valid_in = 1'b0; 
-//             // if (i == 14)
-//             // valid_in = 1'b1; 
-
-            
-//             data_in0 = In_Memory_0[i];
-//             data_in1 = In_Memory_1[i];
-//             data_in2 = In_Memory_2[i];
-//             // in2 = In_Memory[i + 1];
-//             #(k*2);            
-//         end
-//         data_in0 = 32'bx;
-//         data_in1 = 32'bx;
-//         data_in2 = 32'bx;
-//         // -------  Can duy tri VALID_IN them WIDTH+1 clock de co the tinh het so pixel o hang cuoi cung trong anh ---------
-//         #(k*2*(WIDTH + 1));
-        
-//         valid_in = 1'b0;
-//         // #(k*4) $stop;
-//     end
-
-//     // reg [32-1:0] Out_Memory [0:Total_Pixel-1];
-    
-//     reg [32-1:0] Out_Memory_0 [0:Total_Pixel-1];
-//     reg [32-1:0] Out_Memory_1 [0:Total_Pixel-1];
-//     reg [32-1:0] Out_Memory_2 [0:Total_Pixel-1];
-//     reg [32-1:0] Out_Memory_3 [0:Total_Pixel-1];
-//     reg [32-1:0] Out_Memory_4 [0:Total_Pixel-1];
-//     reg [32-1:0] Out_Memory_5 [0:Total_Pixel-1];
-//     reg [32-1:0] Out_Memory_6 [0:Total_Pixel-1];
-//     reg [32-1:0] Out_Memory_7 [0:Total_Pixel-1];
-//     integer j;
-//     initial 
-//     begin
-//         j = 0;
-//         #(k*3/2);
-
-//         while (valid_out == 1'b0)
-//         begin
-//              #(k*2);
-//         end
-
-//         for (j = 0; j < Total_Pixel; j = j + 1)
-//         begin
-//             if (valid_out == 1'b1)
-//                 // Out_Memory[j] = data_out;
-//                 begin
-//                 Out_Memory_0[j] = data_out0;
-//                 Out_Memory_1[j] = data_out1;
-//                 Out_Memory_2[j] = data_out2;
-//                 Out_Memory_3[j] = data_out3;
-//                 Out_Memory_4[j] = data_out4;
-//                 Out_Memory_5[j] = data_out5;
-//                 Out_Memory_6[j] = data_out6;
-//                 Out_Memory_7[j] = data_out7;
-//                 end
-//             else
-//                 // Out_Memory[j] = 32'hx;
-//                 begin
-//                 Out_Memory_0[j] = 32'hx;
-//                 Out_Memory_1[j] = 32'hx;
-//                 Out_Memory_2[j] = 32'hx;
-//                 Out_Memory_3[j] = 32'hx;
-//                 Out_Memory_4[j] = 32'hx;
-//                 Out_Memory_5[j] = 32'hx;
-//                 Out_Memory_6[j] = 32'hx;
-//                 Out_Memory_7[j] = 32'hx;
-//                 end
-//             #(k*2);
-//         end
-
-//         #(k*2);
-//         // $writememh(Outfile, Out_Memory);
-
-//         $writememh(Outfile_0, Out_Memory_0);
-//         $writememh(Outfile_1, Out_Memory_1);
-//         $writememh(Outfile_2, Out_Memory_2);
-//         $writememh(Outfile_3, Out_Memory_3);
-//         $writememh(Outfile_4, Out_Memory_4);
-//         $writememh(Outfile_5, Out_Memory_5);
-//         $writememh(Outfile_6, Out_Memory_6);
-//         $writememh(Outfile_7, Out_Memory_7);
-
-//         #k $stop;
-//     end
-    
-//     always #k clk = ~clk;
-
-//     // testbench cho mach conv2d_8kernel_3channel
-//     block1_conv1_8_kernel_3_channel #(
-// 		.DATA_WIDTH(32),.IMG_WIDTH(WIDTH),.IMG_HEIGHT(HEIGHT)
-// 		)
-// 		block1_conv1_8kernel_3channel(
-// 		clk,
-// 		resetn,
-// 		valid_in,
-// 		data_in0,             
-// 		data_in1,             
-// 		data_in2,             
-// 		// load_kernel0,
-// 		// kernel,            
-// 		// data_out,
-//         data_out0,
-//         data_out1,
-//         data_out2,
-//         data_out3,
-//         data_out4,
-//         data_out5,
-//         data_out6,
-//         data_out7,
-// 		valid_out,
-// 		done
-// 		// load_kernel_done_0
-// 		);
-
-// endmodule
